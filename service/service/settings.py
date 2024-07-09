0c54c3b73bd88c7390f673360efe88084d7bf47d
darlyncer <darlync3d@gmail.com>
2024-07-09 14:26:53 +0500

    day 7-1 create blog

--- service/service/settings.py
+++ service/service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'clients',
     'services',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Для разработки
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Для продакшн
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
